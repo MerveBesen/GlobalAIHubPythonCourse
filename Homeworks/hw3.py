75420879b1cc02c2647a381c1eabb41df88e6f5d
MerveBesen <61238263+MerveBesen@users.noreply.github.com>
2021-03-13 00:07:14 +0300

    Add files via upload

---
+++ Homeworks/hw3.py
@@ -0,0 +1,34 @@
+student = {}
+grade = list()
+studentGrades = list()
+
+for i in range(5) :
+    
+    j = list()
+    name = input("Name : ")
+    j.append(name)
+    surname = input("Surname : ")
+    j.append(surname)
+    midterm = int(input("Midterm : "))
+    j.append(midterm)
+    project = int(input("Project : "))
+    j.append(project)
+    final = int(input("Final : "))
+    j.append(final)
+    passingGrade = midterm * 0.3 + project * 0.3 + final * 0.4
+    j.append(passingGrade)
+
+    grade.append(passingGrade)
+    student[i] = j
+    
+sortedGrade = grade.copy()
+sortedGrade.sort(reverse=True)
+
+for i in sortedGrade:
+    a = grade.index(i)
+
+    studentGrades.append(student.get(a))
+
+    print("Student : ", student.get(a)[0], "Grade : ", i)
+
+print(studentGrades)
